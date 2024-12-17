0d619f4748e048df5a2333fc9784d755e6162f0f
Shaikh Mohammad Saad <shaikhmdsaad92@gmail.com>
2024-12-17 19:54:40 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{  // mongo me agar @ pass mein rahega to uski cencoding likhne ka jaise mere mein %40 tha
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.error(err);
+});
 const app= express();
 
 app.listen(3000, ()=>{
