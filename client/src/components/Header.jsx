f92ff7863d4367fc495ad7da704b650cc334d5a2
Shaikh Mohammad Saad <shaikhmdsaad92@gmail.com>
2025-01-12 14:09:06 +0530

    complete about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,21 +5,21 @@ import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(location.search);
-    urlParams.set('searchTerm', searchTerm);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
+    const searchTermFromUrl = urlParams.get("searchTerm");
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
@@ -27,12 +27,24 @@ export default function Header() {
 
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between  items-center max-w-6xl mx-auto p-3">
         <h1 className="font-bold text-sm sm:text-xl flex-wrap">
-          <span className="text-slate-500">Sahand</span>
-          <span className="text-slate-700">Estate</span>
+        
+        <div className="flex items-center justify-center gap-2">
+        <img  className="w-7 " src="/favicon-96x96.png" alt="" />
+        
+
+      
+        <span className="text-slate-500">FindYour</span>
+        <span className="text-slate-700"> Home</span>
+        </div>
+       
+
         </h1>
-        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="search"
             placeholder="Search.."
@@ -57,7 +69,11 @@ export default function Header() {
           </Link>
           <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
             ) : (
               <li className="text-slate-700 hover:underline hover:shadow-lg hover:scale-110 duration-300">
                 SignIn
